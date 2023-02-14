e6d1fa7e5e0a1369333908175f1716dc8c397e1c
Vaibhav Matere <vpm00718897@gmail.com>
2023-02-15 01:38:02 +0530

    refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
       <h1>Home Component</h1>
+
+      <Link to="/projects">
+        <button>Check Project</button>
+      </Link>
     </>
   );
 };
