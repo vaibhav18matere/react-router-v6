0b6865d4de754ddd22db259dac4dd19d5895f2eb
Vaibhav Matere <vpm00718897@gmail.com>
2023-02-14 11:36:42 +0530

    <Navigate/> and replace to="/"

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
 import Home from "./components/Home";
+import Projects from "./components/Projects";
 import reportWebVitals from "./reportWebVitals";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,6 +19,11 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route
+          path="/my-projects"
+          element={<Navigate replace to="/projects" />}
+        />
       </Routes>
     </Router>
   </React.StrictMode>
